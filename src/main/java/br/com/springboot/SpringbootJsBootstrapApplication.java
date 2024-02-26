4d452acb94cc6671c1919a78c1d4ca8916916ca7
Carlos Eduardo <eduardolcmdeveloper@gmail.com>
2024-02-26 17:48:48 -0300

    projeto base

---
+++ src/main/java/br/com/springboot/SpringbootJsBootstrapApplication.java
@@ -0,0 +1,13 @@
+package br.com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJsBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJsBootstrapApplication.class, args);
+	}
+
+}
